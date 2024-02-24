401cbfea6201ee3c296c6625db8314fae205c65e
Zeshan <zeshanzahid@hotmail.com>
2024-02-23 19:52:23 -0500

    The nextjs app directory was bugged so had to make a new one and transfer files over

--- frontend/src/app/page.js
+++ frontend/src/app/page.js
@@ -1,12 +1,11 @@
 import React from 'react';
 import Signin from './signin'; // Adjust the path as necessary
-import Setin from './setpage/setin'; // Adjust the path as necessary
 //<Signin /> {/* Render the Signin component */}
 export default function Home() {
   return (
     <main className="flex min-h-screen flex-col items-center justify-center p-24">
+      <Signin /> {/* Render the Signin component */}
       
-      <Setin /> {/* Render the Setin component */}
       
       {/* The rest of your page content goes here, or you can replace the existing content with the Signin component */}
     </main>
