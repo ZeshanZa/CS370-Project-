2b3e4e99dfe592a6f98fd599d198cb79238e9b5e
Zeshan <zeshanzahid@hotmail.com>
2024-03-02 02:56:05 -0500

    Added database PostgreSQL

--- backend/django_proj/settings.py
+++ backend/django_proj/settings.py
@@ -74,9 +74,13 @@ WSGI_APPLICATION = "django_proj.wsgi.application"
 # https://docs.djangoproject.com/en/4.2/ref/settings/#databases
 
 DATABASES = {
-    "default": {
-        "ENGINE": "django.db.backends.sqlite3",
-        "NAME": BASE_DIR / "db.sqlite3",
+    'default': {
+        'ENGINE': 'django.db.backends.postgresql_psycopg2',
+        'NAME': 'userinfop', 
+        'USER': 'postgres',
+        'PASSWORD': 'admin1',
+        'HOST': '127.0.0.1', 
+        'PORT': '5432',
     }
 }
 
