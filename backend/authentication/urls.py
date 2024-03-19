c5ca1adbf27416243cfbacb4e215178b06d902ba
eting4 <128740931+eting4@users.noreply.github.com>
2024-03-19 13:30:41 -0400

    urls.py changes

--- backend/authentication/urls.py
+++ backend/authentication/urls.py
@@ -2,7 +2,9 @@ from dj_rest_auth.jwt_auth import get_refresh_view
 from dj_rest_auth.registration.views import RegisterView
 from dj_rest_auth.views import LoginView, LogoutView, UserDetailsView
 from django.urls import path
+from django.contrib.auth import views as auth_views 
 from rest_framework_simplejwt.views import TokenVerifyView
+
 
 urlpatterns = [
     path("register/", RegisterView.as_view(), name="rest_register"),
@@ -11,4 +13,5 @@ urlpatterns = [
     path("user/", UserDetailsView.as_view(), name="rest_user_details"),
     path("token/verify/", TokenVerifyView.as_view(), name="token_verify"),
     path("token/refresh/", get_refresh_view().as_view(), name="token_refresh"),
+    
 ]
