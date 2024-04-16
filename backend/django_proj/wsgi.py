4bd60a3e54b6b248e10a8bbe5aee675042cc1703
Zeshan <zeshanzahid@hotmail.com>
2024-04-16 15:08:28 -0400

    Vercel changes

--- backend/django_proj/wsgi.py
+++ backend/django_proj/wsgi.py
@@ -7,10 +7,9 @@ For more information on this file, see
 https://docs.djangoproject.com/en/4.2/howto/deployment/wsgi/
 """
 
+
 import os
-
 from django.core.wsgi import get_wsgi_application
-
-os.environ.setdefault("DJANGO_SETTINGS_MODULE", "django_proj.settings")
-
+os.environ.setdefault("DJANGO_SETTINGS_MODULE", "django_blog.settings")
 application = get_wsgi_application()
+app = application
