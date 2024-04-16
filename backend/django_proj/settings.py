f21de94818bc8798a610aad2abf9547043231b52
Esmé Taylor R <116604264+ngholomennod@users.noreply.github.com>
2024-04-16 01:07:41 -0400

    Update settings.py

--- backend/django_proj/settings.py
+++ backend/django_proj/settings.py
@@ -26,7 +26,7 @@ SECRET_KEY = "django-insecure-7$)qq=hu_rhzw$w2(%4i7na$5l$8)jtn-yyyiln2q6debh5y@^
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = [".awsapprunner.com"]
+ALLOWED_HOSTS = []
 
 
 # Application definition
