3490f72d95fb1ef306fe752b3311a0ad11281b99
Zeshan <zeshanzahid@hotmail.com>
2024-04-16 15:26:05 -0400

    Undo vercel changes

--- backend/django_proj/wsgi.py
+++ backend/django_proj/wsgi.py
@@ -10,6 +10,6 @@ https://docs.djangoproject.com/en/4.2/howto/deployment/wsgi/
 
 import os
 from django.core.wsgi import get_wsgi_application
-os.environ.setdefault("DJANGO_SETTINGS_MODULE", "django_blog.settings")
+os.environ.setdefault("DJANGO_SETTINGS_MODULE", "django_proj.settings")
 application = get_wsgi_application()
-app = application
+
