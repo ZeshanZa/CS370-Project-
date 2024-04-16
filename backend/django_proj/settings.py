2015b69ba98249b273869d5db4f571620c731035
Esmé Taylor R <116604264+ngholomennod@users.noreply.github.com>
2024-04-16 01:09:16 -0400

    Update settings.py

--- backend/django_proj/settings.py
+++ backend/django_proj/settings.py
@@ -86,10 +86,10 @@ CORS_ALLOW_CREDENTIALS = True
 
 
 CORS_ORIGIN_WHITELIST = [
-    'http://localhost:3000',
+    'http://localhost:3000'"https://cs-370-project.vercel.app",
 ]
 
-CSRF_TRUSTED_ORIGINS = ['http://localhost:3000',]
+CSRF_TRUSTED_ORIGINS = ['http://localhost:3000',"https://cs-370-project.vercel.app"]
 CORS_ALLOWED_ORIGINS = [
         "http://localhost:3000",
         "http://127.0.0.1:8000",
