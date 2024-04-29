17ce9f73274ab5a0a33772c6c9876e295bb7d6ba
eting4 <128740931+eting4@users.noreply.github.com>
2024-04-29 17:35:10 -0400

    match decline models

--- backend/matches/admin.py
+++ backend/matches/admin.py
@@ -1,5 +1,6 @@
 from django.contrib import admin
-from .models import Match 
+from .models import Match, DeclinedMatch
 
 # Register your models here.
 admin.site.register(Match) 
+admin.site.register(DeclinedMatch)
--- backend/matches/models.py
+++ backend/matches/models.py
@@ -13,3 +13,19 @@ class Match(models.Model):
     
     def __str__(self):
         return f"{self.sender.username} and {self.receiver.username} - {self.status}"
+    
+class DeclinedMatch(models.Model):
+    sender = models.ForeignKey(
+        settings.AUTH_USER_MODEL,
+        related_name='declined_match_requests_sent',
+        on_delete=models.CASCADE
+    )
+    receiver = models.ForeignKey(
+        settings.AUTH_USER_MODEL,
+        related_name='declined_match_requests_received',
+        on_delete=models.CASCADE
+    )
+    declined_at = models.DateTimeField(auto_now_add=True)
+
+    def __str__(self):
+        return f"Declined match: {self.sender.username} to {self.receiver.username}"
